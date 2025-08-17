20b7368385ffb79c2c2531d4b6b9e221725e3ca6
martin reda <martinreda999@outlook.com>
2025-08-17 05:37:54 +0300

    Q01

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Assignment02
+﻿using System.Collections;
+
+namespace Assignment02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+
+            string[] firstLine = Console.ReadLine().Split();
+            int n = int.Parse(firstLine[0]);
+            int q = int.Parse(firstLine[1]);
+
+            
+            ArrayList arr = new ArrayList();
+            string[] numbers = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(int.Parse(numbers[i]));
+            }
+
+           
+            for (int i = 0; i < q; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                int count = 0;
+
+                // Linear Search
+                for (int j = 0; j < arr.Count; j++)
+                {
+                    if ((int)arr[j] > x)  
+                        count++;
+                }
+
+                Console.WriteLine(count);
+            }
+
+            #endregion
         }
     }
 }
