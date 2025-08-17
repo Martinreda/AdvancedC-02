fd504497f25e5d4306957573695d767ed1237a24
martin reda <martinreda999@outlook.com>
2025-08-17 05:42:10 +0300

    Q02

--- Assignment02/Program.cs
+++ Assignment02/Program.cs
@@ -8,35 +8,61 @@ namespace Assignment02
         {
             #region Q01
 
-            string[] firstLine = Console.ReadLine().Split();
-            int n = int.Parse(firstLine[0]);
-            int q = int.Parse(firstLine[1]);
+            //string[] firstLine = Console.ReadLine().Split();
+            //int n = int.Parse(firstLine[0]);
+            //int q = int.Parse(firstLine[1]);
 
             
-            ArrayList arr = new ArrayList();
+            //ArrayList arr = new ArrayList();
+            //string[] numbers = Console.ReadLine().Split();
+            //for (int i = 0; i < n; i++)
+            //{
+            //    arr.Add(int.Parse(numbers[i]));
+            //}
+
+           
+            //for (int i = 0; i < q; i++)
+            //{
+            //    int x = int.Parse(Console.ReadLine());
+            //    int count = 0;
+
+            //    // Linear Search
+            //    for (int j = 0; j < arr.Count; j++)
+            //    {
+            //        if ((int)arr[j] > x)  
+            //            count++;
+            //    }
+
+            //    Console.WriteLine(count);
+            //}
+
+            #endregion
+
+            #region Q02 
+
+            int n = int.Parse(Console.ReadLine());
             string[] numbers = Console.ReadLine().Split();
+
+            List<int> arr = new List<int>();
+
+            
             for (int i = 0; i < n; i++)
             {
                 arr.Add(int.Parse(numbers[i]));
             }
 
-           
-            for (int i = 0; i < q; i++)
-            {
-                int x = int.Parse(Console.ReadLine());
-                int count = 0;
+            
+            List<int> reversed = new List<int>(arr);
+            reversed.Reverse();
 
-                // Linear Search
-                for (int j = 0; j < arr.Count; j++)
-                {
-                    if ((int)arr[j] > x)  
-                        count++;
-                }
-
-                Console.WriteLine(count);
-            }
-
+            
+            if (arr.SequenceEqual(reversed))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
             #endregion
+
+
         }
     }
 }
